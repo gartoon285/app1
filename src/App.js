465a940f5feaee41db7a9fe0ab71e449cb8ae8c1
gartoon285 <gartoon285@gmail.com>
2024-01-09 14:41:21 +0700

    Add component
    Hi teacher

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React from 'react'
-import './style.css'
-function App() {
-    return (
-        <>
-        <h2>DevelopThai.com</h2>
-        <div className='title'>React & React Native</div>
-        </>
-    )
-}
-
-export default App;
+import React from 'react'; 
+import { Content, Footer, Header } from './func-components.js'; 
+import Calendar from './class-components.js'; 
+function App() { 
+    return (<> <Header /> <p><center><Calendar /></center></p> <Content /> <Footer /> </>)
+ } 
+ export default App;
