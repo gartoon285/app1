5a6e53ae71305af3c31f72627a213be44f904d99
gartoon285 <gartoon285@gmail.com>
2024-02-19 10:01:51 +0700

    form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Router3 from "./router3";
+import FormSearch from "./form-search";
 
 export default function App() {
-  return <Router3 />;
+  return <FormSearch />;
 }
